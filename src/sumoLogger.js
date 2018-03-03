92d098cadcd2a6ec10dceaa4f425d5cf21764c42
Clement Allen <clementallen313@gmail.com>
2018-03-03 21:41:24 +0000

    Added linting and fixed errors

--- src/sumoLogger.js
+++ src/sumoLogger.js
@@ -1,192 +1,200 @@
-var request = require('request');
-var _ = require('underscore');
+'use strict';
 
-var DEFAULT_INTERVAL = 0;
-var NOOP = () => {};
+const request = require('request');
+const _ = require('underscore');
 
-var originalOpts = {};
-var currentConfig = {};
-var currentLogs = [];
-var interval;
+const DEFAULT_INTERVAL = 0;
+const NOOP = () => {};
+
+let currentConfig = {};
+let currentLogs = [];
+let interval;
 
 function getUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var piece = Math.random() * 16 | 0;
-    var elem = c == 'x' ? piece : (piece & 0x3 | 0x8);
-    return elem.toString(16);
-  });
+    // eslint gets funny about bitwise
+    /* eslint-disable */
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        const piece = Math.random() * 16 | 0;
+        const elem = c === 'x' ? piece : (piece & 0x3 | 0x8);
+        return elem.toString(16);
+    });
+    /* eslint-enable */
 }
 
-function setConfig(opts) {
-  currentConfig = {
-    endpoint: opts.endpoint,
-    clientUrl: opts.clientUrl || '',
-    interval: opts.interval || DEFAULT_INTERVAL,
-    sourceName: opts.sourceName || '',
-    hostName: opts.hostName || '',
-    sourceCategory: opts.sourceCategory || '',
-    session: opts.sessionKey || getUUID(),
-    onSuccess: opts.onSuccess || NOOP,
-    onError: opts.onError || NOOP
-  };
+function setConfig(config) {
+    currentConfig = {
+        endpoint: config.endpoint,
+        clientUrl: config.clientUrl || '',
+        interval: config.interval || DEFAULT_INTERVAL,
+        sourceName: config.sourceName || '',
+        hostName: config.hostName || '',
+        sourceCategory: config.sourceCategory || '',
+        session: config.sessionKey || getUUID(),
+        onSuccess: config.onSuccess || NOOP,
+        onError: config.onError || NOOP
+    };
 }
 
 function sendLogs() {
-  if (currentLogs.length === 0) {
-    return;
-  }
-  var tempCategory = '';
-  var logsToSend;
-
-  try {
-    var headers = {'Content-Type': 'application/json'};
-    if (currentConfig.sourceName !== '') {
-      _.extend(headers, {'X-Sumo-Name': currentConfig.sourceName});
-    }
-    if (currentConfig.sourceCategory !== '') {
-      _.extend(headers, {'X-Sumo-Category': currentConfig.sourceCategory});
-    }
-    if (currentConfig.hostName !== '') {
-      _.extend(headers, {'X-Sumo-Host': currentConfig.hostName});
+    if (currentLogs.length === 0) {
+        return;
     }
 
-    logsToSend = currentLogs;
-    currentLogs = [];
-    request({
-      method: 'POST',
-      url: currentConfig.endpoint,
-      headers: headers,
-      body: logsToSend.join('\n')
-    }, function (error, response) {
-      var err = !!error || response.statusCode < 200 || response.statusCode >= 400;
+    let logsToSend;
 
-      if (err) {
+    try {
+        const headers = { 'Content-Type': 'application/json' };
+        if (currentConfig.sourceName !== '') {
+            _.extend(headers, { 'X-Sumo-Name': currentConfig.sourceName });
+        }
+        if (currentConfig.sourceCategory !== '') {
+            _.extend(headers, { 'X-Sumo-Category': currentConfig.sourceCategory });
+        }
+        if (currentConfig.hostName !== '') {
+            _.extend(headers, { 'X-Sumo-Host': currentConfig.hostName });
+        }
+
+        logsToSend = currentLogs;
+        currentLogs = [];
+
+        request({
+            method: 'POST',
+            url: currentConfig.endpoint,
+            headers,
+            body: logsToSend.join('\n')
+        }, (error, response) => {
+            const err = !!error || response.statusCode < 200 || response.statusCode >= 400;
+
+            if (err) {
+                currentLogs = logsToSend;
+                currentConfig.onError();
+            } else {
+                currentConfig.onSuccess();
+            }
+        });
+    } catch (ex) {
         currentLogs = logsToSend;
         currentConfig.onError();
-      } else {
-          currentConfig.onSuccess();
-      }
-    });
-  } catch (ex) {
-    currentLogs = logsToSend;
-    currentConfig.onError();
-  }
+    }
 }
 
 function SumoLogger(opts) {
-  if (!opts || !opts.hasOwnProperty('endpoint') || opts.endpoint === undefined || opts.endpoint === '') {
-    console.error('Sumo Logic Logger requires you to set an endpoint.');
-    return;
-  }
+    if (!opts || !Object.prototype.hasOwnProperty.call(opts, 'endpoint') || opts.endpoint === undefined || opts.endpoint === '') {
+        console.error('Sumo Logic Logger requires you to set an endpoint.');
+        return;
+    }
 
-  originalOpts = opts;
-  setConfig(opts);
-  this.startLogSending();
+    setConfig(opts);
+    this.startLogSending();
 }
 
-SumoLogger.prototype.updateConfig = function (newOpts) {
-  try {
-    if (!_.isEmpty(newOpts)) {
-      if (newOpts.endpoint) {
-        currentConfig.endpoint = newOpts.endpoint;
-      }
-      if (newOpts.interval) {
-        currentConfig.interval = newOpts.interval;
-        this.startLogSending();
-      }
-      if (newOpts.sourceCategory) {
-        currentConfig.sourceCategory = newOpts.sourceCategory;
-      }
+SumoLogger.prototype.updateConfig = (newOpts) => {
+    try {
+        if (!_.isEmpty(newOpts)) {
+            if (newOpts.endpoint) {
+                currentConfig.endpoint = newOpts.endpoint;
+            }
+            if (newOpts.interval) {
+                currentConfig.interval = newOpts.interval;
+                SumoLogger.prototype.startLogSending();
+            }
+            if (newOpts.sourceCategory) {
+                currentConfig.sourceCategory = newOpts.sourceCategory;
+            }
+        }
+    } catch (ex) {
+        console.error('Could not update Sumo Logic config');
+        return false;
     }
-  } catch (ex) {
-    console.error('Could not update Sumo Logic config');
-    return false;
-  }
-  return true;
+    return true;
 };
 
-SumoLogger.prototype.emptyLogQueue = function () {
-  currentLogs = [];
+SumoLogger.prototype.emptyLogQueue = () => {
+    currentLogs = [];
 };
 
-SumoLogger.prototype.flushLogs = function () {
-  sendLogs();
+SumoLogger.prototype.flushLogs = () => {
+    sendLogs();
 };
 
-SumoLogger.prototype.startLogSending = function() {
+SumoLogger.prototype.startLogSending = () => {
     if (currentConfig.interval > 0) {
-        interval = setInterval(function() {
+        interval = setInterval(() => {
             sendLogs();
         }, currentConfig.interval);
     }
-}
+};
 
-SumoLogger.prototype.stopLogSending = function() {
+SumoLogger.prototype.stopLogSending = () => {
     clearInterval(interval);
-}
+};
 
-SumoLogger.prototype.log = function(msg, optConfig) {
-  if (!msg) {
-    console.error('Sumo Logic Logger requires that you pass a value to log.');
-    return;
-  }
+SumoLogger.prototype.log = (msg, optionalConfig) => {
+    let message = msg;
 
-  var isArray = msg instanceof Array;
-  var testEl = isArray ? msg[0] : msg;
-  var type = typeof testEl;
-
-  if (type === 'undefined') {
-    console.error('Sumo Logic Logger requires that you pass a value to log.');
-    return;
-  } else if (type === 'object') {
-    if (Object.keys(msg).length === 0) {
-      console.error('Sumo Logic Logger requires that you pass a non-empty JSON object to log.');
-      return;
-    }
-  }
-
-  if (!isArray) {
-    msg = [msg];
-  }
-
-  var ts = new Date();
-  var sessKey = currentConfig.session;
-  var client = {url: currentConfig.clientUrl};
-  if (optConfig) {
-    if (optConfig.hasOwnProperty('sessionKey')) {
-      sessKey = optConfig.sessionKey;
+    if (!message) {
+        console.error('Sumo Logic Logger requires that you pass a value to log.');
+        return;
     }
 
-    if (optConfig.hasOwnProperty('timestamp')) {
-      ts = optConfig.timestamp;
+    const isArray = message instanceof Array;
+    const testEl = isArray ? message[0] : message;
+    const type = typeof testEl;
+
+    if (type === 'undefined') {
+        console.error('Sumo Logic Logger requires that you pass a value to log.');
+        return;
+    } else if (type === 'object') {
+        if (Object.keys(message).length === 0) {
+            console.error('Sumo Logic Logger requires that you pass a non-empty JSON object to log.');
+            return;
+        }
     }
 
-    if (optConfig.hasOwnProperty('url')) {
-      client.url = optConfig.url
+    if (!isArray) {
+        message = [message];
     }
-  }
-  timestamp = ts.toUTCString();
 
-  var msgs = msg.map(function (item) {
-    if (typeof item === "string") {
-      return JSON.stringify(_.extend({
-        msg:       item,
-        sessionId: sessKey,
-        timestamp: timestamp
-      }, client));
-    } else {
-      var curr = {
-        sessionId: sessKey,
-        timestamp: timestamp
-      };
-      return JSON.stringify(_.extend(curr, client, item));
+    let ts = new Date();
+    let sessKey = currentConfig.session;
+    const client = { url: currentConfig.clientUrl };
+
+    if (optionalConfig) {
+        if (Object.prototype.hasOwnProperty.call(optionalConfig, 'sessionKey')) {
+            sessKey = optionalConfig.sessionKey;
+        }
+
+        if (Object.prototype.hasOwnProperty.call(optionalConfig, 'timestamp')) {
+            ts = optionalConfig.timestamp;
+        }
+
+        if (Object.prototype.hasOwnProperty.call(optionalConfig, 'url')) {
+            client.url = optionalConfig.url;
+        }
     }
-  });
 
-  currentLogs = currentLogs.concat(msgs);
-  if (currentConfig.interval === 0) {
-    sendLogs();
-  }
+    const timestamp = ts.toUTCString();
+
+    const messages = message.map((item) => {
+        if (typeof item === 'string') {
+            return JSON.stringify(_.extend({
+                msg: item,
+                sessionId: sessKey,
+                timestamp
+            }, client));
+        }
+        const current = {
+            sessionId: sessKey,
+            timestamp
+        };
+        return JSON.stringify(_.extend(current, client, item));
+    });
+
+    currentLogs = currentLogs.concat(messages);
+
+    if (currentConfig.interval === 0) {
+        sendLogs();
+    }
 };
 
 module.exports = SumoLogger;
