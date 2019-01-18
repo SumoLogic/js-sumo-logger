c077ef4fa4b5049fe7ad55b712895c09cf354e32
Clement Allen <clementallen313@gmail.com>
2019-01-18 21:23:16 +0000

    Fix #55 and removed lodash.assignin

--- src/sumoLogger.js
+++ src/sumoLogger.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const assignIn = require('lodash.assignin');
 const formatDate = require('./formatDate');
 
 const DEFAULT_INTERVAL = 0;
@@ -9,8 +8,8 @@ function getUUID() {
     // eslint gets funny about bitwise
     /* eslint-disable */
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        const piece = Math.random() * 16 | 0;
-        const elem = c === 'x' ? piece : (piece & 0x3 | 0x8);
+        const piece = (Math.random() * 16) | 0;
+        const elem = c === 'x' ? piece : (piece & 0x3) | 0x8;
         return elem.toString(16);
     });
     /* eslint-enable */
@@ -18,7 +17,12 @@ function getUUID() {
 
 class SumoLogger {
     constructor(options) {
-        if (!options || !Object.prototype.hasOwnProperty.call(options, 'endpoint') || options.endpoint === undefined || options.endpoint === '') {
+        if (
+            !options ||
+            !Object.prototype.hasOwnProperty.call(options, 'endpoint') ||
+            options.endpoint === undefined ||
+            options.endpoint === ''
+        ) {
             console.error('An endpoint value must be provided');
             return;
         }
@@ -34,7 +38,12 @@ class SumoLogger {
     setConfig(newConfig) {
         this.config = {
             endpoint: newConfig.endpoint,
-            returnPromise: Object.prototype.hasOwnProperty.call(newConfig, 'returnPromise') ? newConfig.returnPromise : true,
+            returnPromise: Object.prototype.hasOwnProperty.call(
+                newConfig,
+                'returnPromise'
+            )
+                ? newConfig.returnPromise
+                : true,
             clientUrl: newConfig.clientUrl || '',
             interval: newConfig.interval || DEFAULT_INTERVAL,
             sourceName: newConfig.sourceName || '',
@@ -69,50 +78,53 @@ class SumoLogger {
             return false;
         }
 
-        let logsToSend;
-
         try {
             const headers = {
                 'X-Sumo-Client': 'sumo-javascript-sdk'
             };
             if (this.config.graphite) {
-                assignIn(headers, { 'Content-Type': 'application/vnd.sumologic.graphite' });
+                Object.assign(headers, {
+                    'Content-Type': 'application/vnd.sumologic.graphite'
+                });
             } else {
-                assignIn(headers, { 'Content-Type': 'application/json' });
+                Object.assign(headers, { 'Content-Type': 'application/json' });
             }
             if (this.config.sourceName !== '') {
-                assignIn(headers, { 'X-Sumo-Name': this.config.sourceName });
+                Object.assign(headers, {
+                    'X-Sumo-Name': this.config.sourceName
+                });
             }
             if (this.config.sourceCategory !== '') {
-                assignIn(headers, { 'X-Sumo-Category': this.config.sourceCategory });
+                Object.assign(headers, {
+                    'X-Sumo-Category': this.config.sourceCategory
+                });
             }
             if (this.config.hostName !== '') {
-                assignIn(headers, { 'X-Sumo-Host': this.config.hostName });
+                Object.assign(headers, { 'X-Sumo-Host': this.config.hostName });
             }
 
-            logsToSend = this.pendingLogs;
-            this.pendingLogs = [];
-
-            if (this.config.returnPromise && logsToSend.length === 1) {
+            if (this.config.returnPromise && this.pendingLogs.length === 1) {
                 return axios.post(
                     this.config.endpoint,
-                    logsToSend.join('\n'),
-                    { headers }
+                    this.pendingLogs.join('\n'),
+                    {
+                        headers
+                    }
                 );
             }
-            return axios.post(
-                this.config.endpoint,
-                logsToSend.join('\n'),
-                { headers }
-            ).then(() => {
-                logsToSend = [];
-                this.config.onSuccess();
-            }).catch((error) => {
-                this.config.onError(error);
-                this.pendingLogs = logsToSend;
-            });
+
+            return axios
+                .post(this.config.endpoint, this.pendingLogs.join('\n'), {
+                    headers
+                })
+                .then(() => {
+                    this.pendingLogs = [];
+                    this.config.onSuccess();
+                })
+                .catch((error) => {
+                    this.config.onError(error);
+                });
         } catch (ex) {
-            this.pendingLogs = logsToSend;
             this.config.onError(ex);
             return false;
         }
@@ -155,8 +167,14 @@ class SumoLogger {
             return false;
         }
 
-        if (this.config.graphite && (!testEl.path || !testEl.value)) {
-            console.error('Both \'path\' and \'value\' properties must be provided in the message object to send Graphite metrics');
+        if (
+            this.config.graphite &&
+            (!Object.prototype.hasOwnProperty.call(testEl, 'path') ||
+                !Object.prototype.hasOwnProperty.call(testEl, 'value'))
+        ) {
+            console.error(
+                'Both "path" and "value" properties must be provided in the message object to send Graphite metrics'
+            );
             return false;
         }
 
@@ -176,11 +194,21 @@ class SumoLogger {
         const client = { url: this.config.clientUrl };
 
         if (optionalConfig) {
-            if (Object.prototype.hasOwnProperty.call(optionalConfig, 'sessionKey')) {
+            if (
+                Object.prototype.hasOwnProperty.call(
+                    optionalConfig,
+                    'sessionKey'
+                )
+            ) {
                 sessKey = optionalConfig.sessionKey;
             }
 
-            if (Object.prototype.hasOwnProperty.call(optionalConfig, 'timestamp')) {
+            if (
+                Object.prototype.hasOwnProperty.call(
+                    optionalConfig,
+                    'timestamp'
+                )
+            ) {
                 ts = optionalConfig.timestamp;
             }
 
@@ -193,23 +221,30 @@ class SumoLogger {
 
         const messages = message.map((item) => {
             if (this.config.graphite) {
-                return `${item.path} ${item.value} ${Math.round(ts.getTime() / 1000)}`;
+                return `${item.path} ${item.value} ${Math.round(
+                    ts.getTime() / 1000
+                )}`;
             }
             if (this.config.raw) {
                 return item;
             }
             if (typeof item === 'string') {
-                return JSON.stringify(assignIn({
-                    msg: item,
-                    sessionId: sessKey,
-                    timestamp
-                }, client));
+                return JSON.stringify(
+                    Object.assign(
+                        {
+                            msg: item,
+                            sessionId: sessKey,
+                            timestamp
+                        },
+                        client
+                    )
+                );
             }
             const current = {
                 sessionId: sessKey,
                 timestamp
             };
-            return JSON.stringify(assignIn(current, client, item));
+            return JSON.stringify(Object.assign(current, client, item));
         });
 
         this.pendingLogs = this.pendingLogs.concat(messages);
