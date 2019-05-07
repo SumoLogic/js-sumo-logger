7c14047b9945b9298124218b852299ca2175a105
Bill Lazar <blazar@gmail.com>
2019-05-07 10:21:19 -0700

    Add prettier to repo

--- src/sumologic.logger.js
+++ src/sumologic.logger.js
@@ -11,37 +11,39 @@ Please use the sumoLogger.js module instead
 */
 
 (function(window, document) {
-  'use strict';
-  var SESSION_KEY = 'sumologic.logger.session';
+  "use strict";
+  var SESSION_KEY = "sumologic.logger.session";
   var SESSION_KEY_LEN = SESSION_KEY.length + 1;
 
   var currentLogs = [];
 
   function SumoLogger() {
     this.sendErrors = false;
-    this.endpoint = '';
+    this.endpoint = "";
     this.interval = 0;
-    this.session = '';
-    this.timestamp = '';
-    this.hostName = '';
-    this.sourceCategory = '';
-    this.name = '';
+    this.session = "";
+    this.timestamp = "";
+    this.hostName = "";
+    this.sourceCategory = "";
+    this.name = "";
     this.onSuccess = false;
     this.onError = false;
 
-    console.warn('Use of this module is deprecated as of v2.0.0. It will be removed entirely in v3.0.0');
+    console.warn(
+      "Use of this module is deprecated as of v2.0.0. It will be removed entirely in v3.0.0"
+    );
   }
 
   function getUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var piece = Math.random() * 16 | 0;
-      var elem = c === 'x' ? piece : (piece & 0x3 | 0x8);
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+      var piece = (Math.random() * 16) | 0;
+      var elem = c === "x" ? piece : (piece & 0x3) | 0x8;
       return elem.toString(16);
     });
   }
 
   function logToConsole(msg) {
-    if (window && window.console && typeof window.console.log === 'function') {
+    if (window && window.console && typeof window.console.log === "function") {
       console.log(msg);
     }
   }
@@ -49,16 +51,24 @@ Please use the sumoLogger.js module instead
   function setSendError(logger, sendErrors) {
     logger.sendErrors = sendErrors;
 
-    if(logger.sendErrors === true){
+    if (logger.sendErrors === true) {
       var _onerror = window.onerror;
-      window.onerror = function (msg, url, line, col){
-        msg = 'message: ' + msg + ', url: ' + url + ', line_num: ' + line + ', col_num: ' + col;
+      window.onerror = function(msg, url, line, col) {
+        msg =
+          "message: " +
+          msg +
+          ", url: " +
+          url +
+          ", line_num: " +
+          line +
+          ", col_num: " +
+          col;
         logger.log({
-          error: 'BrowserJsException',
+          error: "BrowserJsException",
           exception: msg
         });
 
-        if (_onerror && typeof _onerror === 'function') {
+        if (_onerror && typeof _onerror === "function") {
           _onerror.apply(window, arguments);
         }
       };
@@ -66,22 +76,22 @@ Please use the sumoLogger.js module instead
   }
 
   SumoLogger.prototype = {
-    reset: function () {
+    reset: function() {
       currentLogs = [];
-      this.endpoint = '';
+      this.endpoint = "";
       this.interval = 0;
-      this.session = '';
-      this.timestamp = '';
-      this.hostName = '';
-      this.sourceCategory = '';
-      this.name = '';
+      this.session = "";
+      this.timestamp = "";
+      this.hostName = "";
+      this.sourceCategory = "";
+      this.name = "";
     },
 
-    getCurrentLogs: function () {
+    getCurrentLogs: function() {
       return currentLogs;
     },
 
-    setSession: function (session) {
+    setSession: function(session) {
       if (session) {
         this.session = session;
         this.setCookie(this.session);
@@ -94,14 +104,14 @@ Please use the sumoLogger.js module instead
       }
     },
 
-    config: function (opts) {
-      if (!opts || typeof opts !== 'object') {
-        logToConsole('Sumo Logic Logger requires you to set an endpoint.');
+    config: function(opts) {
+      if (!opts || typeof opts !== "object") {
+        logToConsole("Sumo Logic Logger requires you to set an endpoint.");
         return;
       }
 
-      if (!opts.endpoint || opts.endpoint === '') {
-        logToConsole('Sumo Logic Logger requires you to set an endpoint.');
+      if (!opts.endpoint || opts.endpoint === "") {
+        logToConsole("Sumo Logic Logger requires you to set an endpoint.");
         return;
       }
 
@@ -146,12 +156,16 @@ Please use the sumoLogger.js module instead
     },
 
     log: function(msg, opts) {
-      if (this.endpoint === '') {
-        logToConsole('Sumo Logic Logger requires you to set an endpoint before pushing logs.');
+      if (this.endpoint === "") {
+        logToConsole(
+          "Sumo Logic Logger requires you to set an endpoint before pushing logs."
+        );
         return;
       }
       if (!msg) {
-        logToConsole('Sumo Logic Logger requires that you pass a value to log.');
+        logToConsole(
+          "Sumo Logic Logger requires that you pass a value to log."
+        );
         return;
       }
 
@@ -161,12 +175,16 @@ Please use the sumoLogger.js module instead
       var testEl = isArray ? msg[0] : msg;
       var type = typeof testEl;
 
-      if (type === 'undefined' || (type === 'string' && testEl === '')) {
-        logToConsole('Sumo Logic Logger requires that you pass a value to log.');
+      if (type === "undefined" || (type === "string" && testEl === "")) {
+        logToConsole(
+          "Sumo Logic Logger requires that you pass a value to log."
+        );
         return;
-      } else if (type === 'object') {
+      } else if (type === "object") {
         if (Object.keys(msg).length === 0) {
-          logToConsole('Sumo Logic Logger requires that you pass a non-empty JSON object to log.');
+          logToConsole(
+            "Sumo Logic Logger requires that you pass a non-empty JSON object to log."
+          );
           return;
         }
       }
@@ -188,21 +206,21 @@ Please use the sumoLogger.js module instead
       this.timestamp = ts.toUTCString();
 
       var that = this;
-      var msgs = msg.map(function (item) {
-        if (typeof item === 'string') {
+      var msgs = msg.map(function(item) {
+        if (typeof item === "string") {
           return JSON.stringify({
-            msg:       item,
+            msg: item,
             sessionId: that.session,
-            url:       encodeURI(window.location),
+            url: encodeURI(window.location),
             timestamp: that.timestamp
           });
         } else {
           var curr = {
             sessionId: that.session,
-            url:       encodeURI(window.location),
+            url: encodeURI(window.location),
             timestamp: that.timestamp
           };
-          Object.keys(item).forEach(function (ky) {
+          Object.keys(item).forEach(function(ky) {
             curr[ky] = item[ky];
           });
           return JSON.stringify(curr);
@@ -215,7 +233,7 @@ Please use the sumoLogger.js module instead
       }
     },
 
-    logSent: function () {
+    logSent: function() {
       //no-op
     },
 
@@ -232,7 +250,7 @@ Please use the sumoLogger.js module instead
         currentLogs = [];
 
         var onError = this.onError;
-        xmlHttp.addEventListener('error', function() {
+        xmlHttp.addEventListener("error", function() {
           currentLogs = logsToSend;
           if (onError) {
             onError();
@@ -241,24 +259,24 @@ Please use the sumoLogger.js module instead
 
         if (this.onSuccess) {
           var onSuccess = this.onSuccess;
-          xmlHttp.addEventListener('load', function() {
+          xmlHttp.addEventListener("load", function() {
             onSuccess();
           });
         }
-        xmlHttp.open('POST', this.endpoint + '?callback=logSent', true);
-        xmlHttp.setRequestHeader('Content-Type', 'application/json');
+        xmlHttp.open("POST", this.endpoint + "?callback=logSent", true);
+        xmlHttp.setRequestHeader("Content-Type", "application/json");
         xmlHttp.setRequestHeader("X-Sumo-Client", "sumo-javascript-sdk");
-        if (this.name !== '') {
-          xmlHttp.setRequestHeader('X-Sumo-Name', this.name);
+        if (this.name !== "") {
+          xmlHttp.setRequestHeader("X-Sumo-Name", this.name);
         }
-        if (this.sourceCategory !== '') {
-          xmlHttp.setRequestHeader('X-Sumo-Category', this.sourceCategory);
+        if (this.sourceCategory !== "") {
+          xmlHttp.setRequestHeader("X-Sumo-Category", this.sourceCategory);
         }
-        if (this.hostName !== '') {
-          xmlHttp.setRequestHeader('X-Sumo-Host', this.hostName);
+        if (this.hostName !== "") {
+          xmlHttp.setRequestHeader("X-Sumo-Host", this.hostName);
         }
 
-        xmlHttp.send(logsToSend.join('\n'));
+        xmlHttp.send(logsToSend.join("\n"));
       } catch (ex) {
         currentLogs = logsToSend;
         if (this.onError) {
@@ -267,11 +285,11 @@ Please use the sumoLogger.js module instead
       }
     },
 
-    flushLogs: function () {
+    flushLogs: function() {
       this.sendLogs();
     },
 
-    emptyLogQueue: function () {
+    emptyLogQueue: function() {
       currentLogs = [];
     },
 
@@ -281,21 +299,21 @@ Please use the sumoLogger.js module instead
       if (i < 0) {
         return false;
       } else {
-        var end = cookie.indexOf(';', i + 1);
+        var end = cookie.indexOf(";", i + 1);
         end = end < 0 ? cookie.length : end;
         return cookie.slice(i + SESSION_KEY_LEN, end);
       }
     },
 
     setCookie: function(value) {
-      document.cookie = SESSION_KEY + '=' + value;
+      document.cookie = SESSION_KEY + "=" + value;
     }
   };
 
   var existing = window.SLLogger;
   var logger = new SumoLogger();
 
-  if(existing && existing.length ) {
+  if (existing && existing.length) {
     var i = 0;
     var eLength = existing.length;
     for (i = 0; i < eLength; i++) {
@@ -304,5 +322,5 @@ Please use the sumoLogger.js module instead
   }
 
   window.SLLogger = logger; // default global logger
-  window.SumoLogger = SumoLogger;   // To instantiate additional loggers
+  window.SumoLogger = SumoLogger; // To instantiate additional loggers
 })(window, document);
