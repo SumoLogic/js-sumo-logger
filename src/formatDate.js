db36681432f529fae9d5f260474cc9f87919187e
Bill Lazar <blazar@gmail.com>
2019-07-22 10:45:42 -0700

    Force use of date.toString for stringify

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,5 +1,7 @@
 function formatDate(date) {
-    const timezone = JSON.stringify(date).split('GMT')[1] || '+000';
+    // Per https://stackoverflow.com/a/31096160/2702, stringify on a Date calls
+    // date.toISOString rather than date.toString(), which is what's needed to preserve timestamp
+    const timezone = JSON.stringify(date.toString()).split('GMT')[1] || '+000';
     const asJson = date.toJSON().split('.')[0];
 
     return `${asJson}${timezone}`;
