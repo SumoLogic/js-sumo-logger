69face8529c6b88698e8db621a5dedae3d3b29c2
Bill Lazar <blazar@gmail.com>
2019-07-23 09:44:18 -0700

    Simplify formateDate

--- src/formatDate.js
+++ src/formatDate.js
@@ -1,10 +1,5 @@
 function formatDate(date) {
-    // Per https://stackoverflow.com/a/31096160/2702, stringify on a Date calls
-    // date.toISOString rather than date.toString(), which is what's needed to preserve timestamp
-    const timezone = JSON.stringify(date.toString()).split('GMT')[1] || '+000';
-    const asJson = date.toJSON().split('.')[0];
-
-    return `${asJson}${timezone}`;
+    return date.toJSON();
 }
 
 module.exports = formatDate;
